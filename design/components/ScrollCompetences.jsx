6c6ae9cee08a5ac7c83e2fc91977a63debc457f2
Matzoo38 <mslyemi@gmail.com>
2023-11-24 13:48:45 +0100

    add icons compétences

--- design/components/ScrollCompetences.jsx
+++ design/components/ScrollCompetences.jsx
@@ -3,42 +3,98 @@ import React from "react";
 const ScrollCompetences = () => {
   return (
     <>
-      <div className="relative flex overflow-x-hidden">
-        <div className="py-12 animate-marquee whitespace-nowrap">
-          <span className="text-2xl md:text-4xl mx-4">HTML5 </span>
-          <span className="text-2xl md:text-4xl mx-4">CSS3 </span>
-          <span className="text-2xl md:text-4xl mx-4">JavaScript </span>
-          <span className="text-2xl md:text-4xl mx-4">React </span>
-          <span className="text-2xl md:text-4xl mx-4">NextJS </span>
-          <span className="text-2xl md:text-4xl mx-4">Redux </span>
-          <span className="text-2xl md:text-4xl mx-4">TailwindCSS</span>
-          <span className="text-2xl md:text-4xl mx-4">VsCode</span>
-          <span className="text-2xl md:text-4xl mx-4">Sass</span>
-          <span className="text-2xl md:text-4xl mx-4">GitHub</span>
-          <span className="text-2xl md:text-4xl mx-4">Figma</span>
-          <span className="text-2xl md:text-4xl mx-4">Jest</span>
-          <span className="text-2xl md:text-4xl mx-4">AGILE</span>
-          <span className="text-2xl md:text-4xl mx-4">API-Rest</span>
-          <span className="text-2xl md:text-4xl mx-4">SEO</span>
+      <div className="relative overflow-x-hidden flex">
+        
+      <div className="py-12 animate-marquee whitespace-nowrap flex">
+          
+          <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">HTML5 
+          <img  className="w-20 h-20"   src="/icon/icon-html.png"/>
+          </span>
+          <span className="flex flex-col  items-centertext-2xl md:text-4xl mx-4">CSS3
+          <img  className="w-20 h-20" src="/icon/icon-css.png"/>
+           </span>
+           <span className="flex flex-col  items-center text-2xl md:text-4xl mx-4">Javascript
+          <img   className="w-20 h-20" src="/icon/icon-js.png"/>
+           </span>
+           <span className="flex flex-col  items-center text-2xl md:text-4xl mx-4">React
+          <img   className="w-20 h-20" src="/icon/icon-react.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">NextJs
+          <img   className="w-20 h-20" src="/icon/icon-next.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Redux
+          <img   className="w-20 h-20" src="/icon/icon-redux.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Tailwind
+          <img   className="w-20 h-20" src="/icon/icon-tailwind.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">VsCode
+          <img  className="w-20 h-20" src="/icon/icon-vscode.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Sass
+          <img className="w-20 h-20" src="/icon/icon-sass.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Github
+          <img className="w-20 h-20" src="/icon/icon-github.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Figma
+          <img className="w-20 h-20" src="/icon/icon-figma.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Jest
+          <img className="w-20 h-20" src="/icon/icon-jest.png"/>
+           </span>
+          <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Swagger
+          <img className="w-20 h-20" src="/icon/icon-swagger.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Seo
+          <img className="w-20 h-20" src="/icon/icon-seo.png"/>
+           </span>
           <span className="text-2xl md:text-4xl mx-4"></span>
         </div>
 
-        <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-          <span className="text-2xl md:text-4xl mx-4">HTML5</span>
-          <span className="text-2xl md:text-4xl mx-4">CSS3</span>
-          <span className="text-2xl md:text-4xl mx-4">JavaScript</span>
-          <span className="text-2xl md:text-4xl mx-4">React</span>
-          <span className="text-2xl md:text-4xl mx-4">NextJS</span>
-          <span className="text-2xl md:text-4xl mx-4">Redux</span>
-          <span className="text-2xl md:text-4xl mx-4">TailwindCSS</span>
-          <span className="text-2xl md:text-4xl mx-4">VsCode</span>
-          <span className="text-2xl md:text-4xl mx-4">Sass</span>
-          <span className="text-2xl md:text-4xl mx-4">GitHub</span>
-          <span className="text-2xl md:text-4xl mx-4">Figma</span>
-          <span className="text-2xl md:text-4xl mx-4">Jest</span>
-          <span className="text-2xl md:text-4xl mx-4">AGILE</span>
-          <span className="text-2xl md:text-4xl mx-4">API-Rest</span>
-          <span className="text-2xl md:text-4xl mx-4">SEO</span>
+        <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap flex">
+        <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">HTML5 
+          <img  className="w-20 h-20"   src="/icon/icon-html.png"/>
+          </span>
+          <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">CSS3
+          <img className="w-20 h-20" src="/icon/icon-css.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Javascript 
+          <img  className="w-20 h-20" src="/icon/icon-js.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">React
+          <img  className="w-20 h-20" src="/icon/icon-react.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">NextJs
+          <img  className="w-20 h-20" src="/icon/icon-next.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Redux
+          <img  className="w-20 h-20" src="/icon/icon-redux.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Tailwind
+          <img  className="w-20 h-20" src="/icon/icon-tailwind.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">VsCode
+          <img  className="w-20 h-20" src="/icon/icon-vscode.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Sass
+          <img className="w-20 h-20" src="/icon/icon-sass.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Github
+          <img className="w-20 h-20" src="/icon/icon-github.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Figma
+          <img className="w-20 h-20" src="/icon/icon-figma.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Jest
+          <img className="w-20 h-20" src="/icon/icon-jest.png"/>
+           </span>
+          <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Swagger
+          <img className="w-20 h-20" src="/icon/icon-swagger.png"/>
+           </span>
+           <span className="flex flex-col items-center text-2xl md:text-4xl mx-4">Seo
+          <img className="w-20 h-20" src="/icon/icon-seo.png"/>
+           </span>
           <span className="text-2xl md:text-4xl mx-4"></span>
         </div>
       </div>
