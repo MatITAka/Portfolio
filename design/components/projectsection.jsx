cb93a43f88a3ba8774a41995828fd5f6095cdd45
Matzoo38 <mslyemi@gmail.com>
2024-02-08 11:53:49 +0100

    correctif url github

--- design/components/projectsection.jsx
+++ design/components/projectsection.jsx
@@ -12,8 +12,8 @@ const projectsData = [
     description: "Booki est un site de réservation d'hôtel en ligne, développé en HTML et CSS. Le développement de ce site m'a permis de découvrir mes premières animations CSS ainsi que de découvrir comment fonctionne le responsive design.",
     image :"/images/booki.png",
     tag: ["All", "HTML/CSS"],
-    gitUrl: "https://github.com/Matzoo38/Booki",
-    previewUrl: "https://matzoo38.github.io/Booki/",
+    gitUrl: "https://github.com/matslydev/Booki",
+    previewUrl: "https://matslydev.github.io/Booki/",
   },
   {
     id: 2,
@@ -21,8 +21,8 @@ const projectsData = [
     description: "OhMyFood est un site regroupant des restaurants selon le secteur de votre choix, depuis celui-ci l'on peut consulter les cartes de différents restaurants. Ce projet Mobile First, construit en HTML/CSS, m'a permis d'appronfondir mes connaissances concernant les animations CSS ainsi que découvir une nouvelle approche de responsive design.",
     image: "/images/OMF.png",
     tag: ["All", "HTML/CSS"],
-    gitUrl: "https://github.com/Matzoo38/OhMyFood",
-    previewUrl: "https://matzoo38.github.io/OhMyFood/",
+    gitUrl: "https://github.com/matslydev/OhMyFood",
+    previewUrl: "https://matslydev.github.io/OhMyFood/",
   },
   {
     id: 3,
@@ -30,7 +30,7 @@ const projectsData = [
     description: "Kasa est un site de réservations de chambres d'hôtes type AirBnb. Ce projet construit en React et en Sass m'a permis de découvrir et approfondir ces 2 nouvelles technologies. Ce site m'a permis de prendre conscience de l'immensité du champ des possibles sur React",
     image: "/images/Kasa.png",
     tag: ["All", "React"],
-    gitUrl: "https://github.com/Matzoo38/Kasa",
+    gitUrl: "https://github.com/matslydev/Kasa",
     previewUrl: "https://magical-flan-3f5dbd.netlify.app/",
   },
   {
@@ -39,7 +39,7 @@ const projectsData = [
     description: "77Events est un site de consultation d'évênements en tout genre. Pour ce projet, il s'agit d'un site déliveré buggé avec plusieures erreures majeures. Sur ce projet j'ai dû debbuggé plusieurs fonctions ainsi qu'utiliser Jest qui permet de tester les différents composants de mon application.",
     image: "/images/77Events.png",
     tag: ["All", "React"],
-    gitUrl: "https://github.com/Matzoo38/724Events",
+    gitUrl: "https://github.com/matslydev/724Events",
     previewUrl: "https://dynamic-kelpie-b96c0d.netlify.app/",
   },
   {
@@ -48,7 +48,7 @@ const projectsData = [
     description: "ArgentBank est un site bancaire permettant au client de consulter son compte et de modifier les opérations de celui-ci. Construit en React, il est managé par Redux via un State Global permettant à l'utilisateur connecté de se déplacer librement dans son application, contrairement à celui déconnecté. Il communique via une API qui suit le modèle CRUD lui permettant d'être opérationnel sous chaque type d'opération.",
     image: "/images/ArgentBank.png",
     tag: ["All", "React"],
-    gitUrl: "https://github.com/Matzoo38/ArgentBank-website",
+    gitUrl: "https://github.com/matslydev/ArgentBank-website",
     previewUrl: "https://radiant-vacherin-55a6eb.netlify.app",
   },
   {
@@ -57,7 +57,7 @@ const projectsData = [
     description: "Le portefeuille de Mme Bluel est un portfolio permettant de montrer son travail via des boutons de filtres. L'on a également ajouté un espace de connexion grâce a JavaScript permettant a Mme Buelle de pouvoir changer notamment les photos présentes dans les galleries de présentation via une modale. Il communique via une API qui suit le modèle CRUD lui permettant d'upload et supprimer les photos présentes dans la gallerie.",
     image: "/images/SophieBuell.png",
     tag: ["All", "JavaScript"],
-    gitUrl: "https://github.com/Matzoo38/Sophie-Buell",
+    gitUrl: "https://github.com/matslydev/Sophie-Buell",
     previewUrl: "https://astonishing-scone-f0b118.netlify.app",
   },
 ];
